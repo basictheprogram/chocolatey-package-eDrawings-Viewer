5852c8c68cf2740ad480c433dbb36317e8243e75
Bob Tanner <tanner@real-time.com>
2019-09-24 16:09:44 -0500

    New release of eDrawings-viewer 27.4.0.0020
    Updated
    -------
      * Updated $url64 to the new download location
      * Updated $checksum64
      * Formatting and spacing changes
    
    Fixed
    -----
      * softwareName to uninstall
    
    Deleted
    -------
      * Removed chocolateyBeforeModify.ps1 it's no longer needed

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,36 +1,36 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = $env:ChocolateyPackageName
-$url64       = 'http://dl-ak.solidworks.com/nonsecure/edrawings/e2019sp03/27.3.0.0056-GLWVXMBN/pfw//eDrawingsFullAllX64.exe'
-$checksum64  = '69920b774f308fb42132719cac011924c898a0676c9076857abc31f10f92b769'
-$WorkSpace   = Join-Path $env:TEMP "$packageName.$env:chocolateyPackageVersion"
+$url64 = 'http://dl-ak.solidworks.com/nonsecure/edrawings/e2019sp04/27.4.0.0020-VF1T2ZZJ/pfw//eDrawingsFullAllX64.exe'
+$checksum64 = 'd9eb9834af1c34bef1fdf92c0551f1710c52cd6f02f0da46c0fecc6a73e67e9e'
+$WorkSpace = Join-Path $env:TEMP "$packageName.$env:chocolateyPackageVersion"
 
 $WebFileArgs = @{
-  packageName  = $packageName
-  FileFullPath = Join-Path $WorkSpace "$packageName.exe"
-  Url64bit     = $url64
-  Checksum64   = $checkSum64
-  ChecksumType = 'sha256'
-  GetOriginalFileName = $true
+    packageName         = $packageName
+    FileFullPath        = Join-Path $WorkSpace "$packageName.exe"
+    Url64bit            = $url64
+    Checksum64          = $checkSum64
+    ChecksumType        = 'sha256'
+    GetOriginalFileName = $true
 }
 
 $PackedInstaller = Get-ChocolateyWebFile @WebFileArgs
 
 $UnzipArgs = @{
-  PackageName  = $packageName
-  FileFullPath = $PackedInstaller
-  Destination  = $WorkSpace
+    PackageName  = $packageName
+    FileFullPath = $PackedInstaller
+    Destination  = $WorkSpace
 }
 
 Get-ChocolateyUnzip @UnzipArgs
 
 $InstallArgs = @{
-  PackageName    = $packageName
-  File           = Join-Path $WorkSpace "eDrawingsFullAllX64.exe"
-  fileType       = 'exe'
-  silentArgs     = '/S /v/qn'
-  validExitCodes = @(0, 3010, 1641)
-  softwareName   = 'eDrawings*'
+    PackageName    = $packageName
+    File           = Join-Path $WorkSpace "eDrawingsFullAllX64.exe"
+    fileType       = 'exe'
+    silentArgs     = '/S /v/qn'
+    validExitCodes = @(0, 3010, 1641)
+    softwareName   = 'eDrawings*'
 }
 
 Install-ChocolateyInstallPackage @InstallArgs
